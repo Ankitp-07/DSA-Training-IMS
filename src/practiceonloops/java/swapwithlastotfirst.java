e64a9faefb85057c2160d3e20ca1dfc4aa4af0a0
Ankitpatel-07 <you@example.com>
2025-11-12 11:58:37 +0530

    updated

--- src/practiceonloops/java/swapwithlastotfirst.java
+++ src/practiceonloops/java/swapwithlastotfirst.java
@@ -4,10 +4,19 @@ public class swapwithlastotfirst {
     public static void main(String[] args) {
         int num=12345;
         int rot=2;
+        int copy=num;
+        //counting the digits
+        int countdigit=0;
+        while(num>0){
+            countdigit++;
+            num=num/10;
+        }
+        System.out.println(countdigit);
+        num=copy;
         int div=(int)Math.pow(10,rot);
        int rem= num%div;
        num/=100;
-       num+=rem*1000;
+       num+=rem*(int)Math.pow(10,countdigit-rot);
         System.out.println(num);
 
 
