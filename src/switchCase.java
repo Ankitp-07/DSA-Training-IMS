3a7d7294b883a1ca8200633f625711f4166c9d27
Ankitpatel-07 <you@example.com>
2025-11-13 11:25:15 +0530

    pattern

---
+++ src/switchCase.java
@@ -0,0 +1,20 @@
+public class switchCase {
+    public static void main(String[] args) {
+//        System.out.println("1 . burger");
+//        System.out.println("2 . samosa");
+//        System.out.println("3 . pani patasa");
+//        String ch = "burger";
+//        double price = switch(ch) {
+//            case " burger" -> {
+//                var a = 10;
+//                var b = 20;
+//                var c = a + b * 30;
+//                yield c;
+//            }
+//            case "samosa" -> 200;
+//            case "samosa","pani patasa"-> 300;
+//            default -> 0;
+//
+        //}
+    }
+}
