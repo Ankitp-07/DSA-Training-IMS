e3f733d36197289be4b541319694eafa31f35132
Ankitpatel-07 <you@example.com>
2025-11-14 11:43:55 +0530

    functions

---
+++ src/Recursion/ep1.java
@@ -0,0 +1,10 @@
+package Recursion;
+
+public class ep1 {
+    public static void main(String[] args) {
+        show(5,6);
+    }
+    public static void show(int x,int y){
+        System.out.println(x+" "+y);
+    }
+}
