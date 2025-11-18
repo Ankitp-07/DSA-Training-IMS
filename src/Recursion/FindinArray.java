1c9bd48363a74e5cf2a552eb89c70abb82ce9613
Ankitpatel-07 <you@example.com>
2025-11-18 12:27:37 +0530

    foundNotfound

---
+++ src/Recursion/FindinArray.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class FindinArray {
+    static boolean isFound(int[] arr,int index,int searchValue){
+        if(arr[index]==searchValue) return true;
+        if(arr[index]==arr.length-1) return false;
+        return isFound(arr,index+1,searchValue);
+    }
+
+    public static void main(String[] args) {
+        int[] arr={10,20,30,40,50,60};
+        System.out.println(isFound(arr,0,50) ?"found":"not found");
+
+    }
+}
